53aa58790269b7ed3c3050ecc123fa9924994569
jaq-cat <jaq.cat@protonmail.ch>
2021-07-01 12:26:31 -0300

    done lol

--- src/main.c
+++ src/main.c
@@ -4,11 +4,18 @@
 #define WIDTH 800
 #define HEIGHT 800
 #define BG 200
+#define SIDE 100
 
 SDL_Window *win;
 SDL_Renderer *rend;
 
-int x = 0, y = 0;
+SDL_Point mpos;
+SDL_Rect obj = {
+    0, 0, SIDE, SIDE
+};
+int dragging = 0;
+
+SDL_Texture *boi;
 
 struct {
     int lmb, rmb;
@@ -25,34 +32,57 @@ void init() {
         SDL_WINDOWPOS_CENTERED,
         WIDTH, HEIGHT,
         0);
-    rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    rend = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED);
     IMG_Init(IMG_INIT_PNG);
-}
 
-// rendering
-void render() {
+    boi = IMG_LoadTexture(rend, "res/boi.png");
 }
 
 // processing
 int handleev() {
     SDL_Event event;
-    SDL_PollEvent(&event);
-    switch (event.type) {
-        case SDL_MOUSEBUTTONDOWN:
-            x = event.motion.x;
-            y = event.motion.y;
-            break;
-        case SDL_MOUSEBUTTONUP:
-            x = -1;
-            y = -1;
-            break;
-        default:
-            return 0;
-    }
+    if (SDL_PollEvent(&event)) 
+        switch (event.type) {
+            case SDL_MOUSEMOTION:
+                mpos.x = event.motion.x;
+                mpos.y = event.motion.y;
+                break;
+            case SDL_MOUSEBUTTONDOWN:
+                if (event.button.button == SDL_BUTTON_LEFT)
+                    mouse.lmb = 1;
+                else if (event.button.button == SDL_BUTTON_RIGHT)
+                    mouse.rmb = 1;
+                break;
+            case SDL_MOUSEBUTTONUP:
+                if (event.button.button == SDL_BUTTON_LEFT)
+                    mouse.lmb = 0;
+                else if (event.button.button == SDL_BUTTON_RIGHT)
+                    mouse.rmb = 0;
+                break;
+            default:
+                break;
+        }
     return event.key.keysym.scancode == SDL_SCANCODE_Q;
+
 }
 
 void update() {
+    if (mouse.lmb && SDL_PointInRect(&mpos, &obj)) {
+        dragging = 1;
+    } else {
+        dragging = 0;
+    }
+
+    if (dragging) {
+        obj.x = mpos.x-SIDE/2;
+        obj.y = mpos.y-SIDE/2;
+    }
+}
+
+
+// rendering
+void render() {
+    SDL_RenderCopy(rend, boi, NULL, &obj);
 }
 
 int main() {
